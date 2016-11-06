85a8f12c46e4bb0f1a46077ed001eec486b1571c
komet22 <pawelm642@onet.pl>
2016-11-06 01:36:52 +0100

    Dodanie projektu1 pt2 - Java z szkieletem testów

---
+++ test/main/java/dbh/CRUDJUnitTest.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.java.dbh;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author PM, PJ, SK
+ */
+public class CRUDJUnitTest {
+    
+    public CRUDJUnitTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void addEmployeeTest() {
+        System.out.println("CRUD Create test: addEmployee");
+        assertTrue(true);
+        //TODO
+    }
+    
+    @Test
+    public void listEmployeesTest() {
+        System.out.println("CRUD Read test: listEmployees");
+        assertTrue(true);
+        //TODO
+    }
+    
+    @Test
+    public void updateEmployeeTest() {
+        System.out.println("CRUD Update test: updateEmployee");
+        assertTrue(true);
+        //TODO
+    }
+    
+    @Test
+    public void deleteEmployeeTest() {
+        System.out.println("CRUD Delete test: deleteEmployee");
+        assertTrue(true);
+        //TODO
+    }
+}
---
+++ test/main/java/dbh/DBHTestSuite.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main.java.dbh;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author PM, PJ, SK
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({main.java.dbh.CRUDJUnitTest.class,main.java.dbh.SetStructJUnitTest.class})
+public class DBHTestSuite {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+    
+}
